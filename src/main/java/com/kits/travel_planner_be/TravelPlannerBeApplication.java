21dcc7fd2cac716313c2df542e9ae026612972bb
Việt Long <vietlong@Viets-MacBook-Pro.local>
2023-08-09 16:32:09 +0700

    initial backend project

---
+++ src/main/java/com/kits/travel_planner_be/TravelPlannerBeApplication.java
@@ -0,0 +1,13 @@
+package com.kits.travel_planner_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelPlannerBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelPlannerBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kits/travel_planner_be/TravelPlannerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kits.travel_planner_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelPlannerBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
