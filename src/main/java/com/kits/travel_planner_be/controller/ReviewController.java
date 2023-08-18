2926322b0fe859937a183daee110cbe7357fe760
vdhieu124 <vuongluffy69@gmail.com>
2023-08-18 12:27:41 +0700

    Add api review

---
+++ src/main/java/com/kits/travel_planner_be/controller/ReviewController.java
@@ -0,0 +1,46 @@
+package com.kits.travel_planner_be.controller;
+
+import com.kits.travel_planner_be.payload.request.ReviewRequest;
+import com.kits.travel_planner_be.payload.response.MessageResponse;
+import com.kits.travel_planner_be.payload.response.ResponseSuccess;
+import com.kits.travel_planner_be.payload.response.ReviewResponse;
+import com.kits.travel_planner_be.service.ReviewService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/reviews")
+public class ReviewController {
+    @Autowired
+    private ReviewService reviewService;
+
+    @PostMapping
+    public ResponseEntity<?> addReview(@RequestBody @Valid ReviewRequest reviewRequest){
+        ReviewResponse reviewResponse = reviewService.saveReview(reviewRequest);
+        ResponseSuccess<ReviewResponse> responseSuccess = new ResponseSuccess<>();
+        responseSuccess.setMessage("Created review successful. ");
+        responseSuccess.setData(reviewResponse);
+
+        return new ResponseEntity<>(responseSuccess, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateReview(@PathVariable Long id, @RequestBody @Valid ReviewRequest reviewRequest){
+        ReviewResponse reviewResponse = reviewService.updateReview(id, reviewRequest);
+        ResponseSuccess<ReviewResponse> responseSuccess = new ResponseSuccess<>();
+        responseSuccess.setMessage("Updated review successful. ");
+        responseSuccess.setData(reviewResponse);
+        return new ResponseEntity<>(responseSuccess, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteReviewById (@PathVariable Long id){
+        reviewService.deleteReviewById(id);
+        MessageResponse messageResponse = new MessageResponse(true, "Deleted review with id: " + id);
+
+        return new ResponseEntity<>(messageResponse, HttpStatus.OK);
+    }
+}
--- src/main/java/com/kits/travel_planner_be/model/SharedTrip.java
+++ src/main/java/com/kits/travel_planner_be/model/SharedTrip.java
@@ -19,5 +19,9 @@ public class SharedTrip {
     @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "trip_id")
     private Trip trip;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "shared_user_trip")
+    private User user;
     private Timestamp sharedDate;
 }
